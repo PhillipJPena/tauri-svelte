572df28726f02e6805afbb6e796cecb07c59298b
PhillipJPena <99215362+PhillipJPena@users.noreply.github.com>
2024-03-21 22:52:04 -0400

    added tailwind and shadcn

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,12 @@
 <script lang="ts">
-    import Greet from "./lib/Greet.svelte";
+  import Button from "$lib/components/ui/button/button.svelte";
+  import Input from "$lib/components/ui/input/input.svelte";
 </script>
 
-<main class="container">
-    <h1>Welcome to Heaven!</h1>
-
-    <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-            <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank">
-            <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-        </a>
-    </div>
-
-    <p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
-
-    <div class="row">
-        <Greet />
-    </div>
+<main class="grid place-content-center h-full w-full bg-gray-200 gap-4">
+  <h1 class=" font-bold text-2xl">Hello World!</h1>
+  <div class="flex gap-4">
+    <Input placeholder="What is your name?" class="max-w-xs" />
+    <Button variant="outline" class=" w-20">Submit</Button>
+  </div>
 </main>
-
-<style>
-    .logo.vite:hover {
-        filter: drop-shadow(0 0 2em #747bff);
-    }
-
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00);
-    }
-</style>
